bf7ef077dec66ffc6d9cdf8ac134c77894068857
Raúl Padilla Delgado <rpd27@outlook.com>
2022-01-21 14:25:56 +0000

    refactor

--- src/main/java/MarkdownTransformer.java
+++ src/main/java/MarkdownTransformer.java
@@ -3,12 +3,20 @@ import java.util.regex.Pattern;
 
 public class MarkdownTransformer {
     public String execute(String input) {
+        String firstMatch = findMatch(input);
+        if (!firstMatch.isEmpty()) {
+            return firstMatch;
+        }
+        return input;
+    }
+
+    private String findMatch(String input) {
         Pattern pattern = Pattern.compile("\\[.*]\\(.*\\)");
         Matcher matcher = pattern.matcher(input);
         boolean thereAreLinks = matcher.find();
-        if (thereAreLinks){
+        if (thereAreLinks) {
             return matcher.group();
         }
-        return input;
+        return "";
     }
 }
